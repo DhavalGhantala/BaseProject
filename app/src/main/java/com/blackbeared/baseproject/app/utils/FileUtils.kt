3a3ec35b858385d991ce5fa051d9f8fd1f0f53a3
DhavalGhantala <dhavalghantala68@gmail.com>
2019-12-25 01:07:48 +0530

    Lib project

--- app/src/main/java/com/blackbeared/baseproject/app/utils/FileUtils.kt
+++ app/src/main/java/com/blackbeared/baseproject/app/utils/FileUtils.kt
@@ -38,111 +38,111 @@ class FileUtils(val context: Context) {
         return photo
     }
 
-    fun compressImage(imageUri: String): String {
-        val filePath = getRealPathFromURI(imageUri)
-        var scaledBitmap: Bitmap? = null
-
-        val options = BitmapFactory.Options()
-        options.inJustDecodeBounds = true
-        var bmp = BitmapFactory.decodeFile(filePath, options)
-
-        var actualHeight = options.outHeight
-        var actualWidth = options.outWidth
-
-        val maxHeight = 816.0f
-        val maxWidth = 612.0f
-        var imgRatio = (actualWidth / actualHeight).toFloat()
-        val maxRatio = maxWidth / maxHeight
-
-        if (actualHeight > maxHeight || actualWidth > maxWidth) {
-            when {
-                imgRatio < maxRatio -> {
-                    imgRatio = maxHeight / actualHeight
-                    actualWidth = (imgRatio * actualWidth).toInt()
-                    actualHeight = maxHeight.toInt()
-                }
-                imgRatio > maxRatio -> {
-                    imgRatio = maxWidth / actualWidth
-                    actualHeight = (imgRatio * actualHeight).toInt()
-                    actualWidth = maxWidth.toInt()
-                }
-                else -> {
-                    actualHeight = maxHeight.toInt()
-                    actualWidth = maxWidth.toInt()
-
-                }
-            }
-        }
-
-        options.inSampleSize = calculateInSampleSize(options, actualWidth, actualHeight)
-        options.inJustDecodeBounds = false
-        options.inTempStorage = ByteArray(16 * 1024)
-
-        try {
-            bmp = BitmapFactory.decodeFile(filePath, options)
-        } catch (exception: OutOfMemoryError) {
-            exception.printStackTrace()
-
-        }
-
-        try {
-            scaledBitmap = Bitmap.createBitmap(actualWidth, actualHeight, Bitmap.Config.ARGB_8888)
-        } catch (exception: OutOfMemoryError) {
-            exception.printStackTrace()
-        }
-
-        val ratioX = actualWidth / options.outWidth.toFloat()
-        val ratioY = actualHeight / options.outHeight.toFloat()
-        val middleX = actualWidth / 2.0f
-        val middleY = actualHeight / 2.0f
-
-        val scaleMatrix = Matrix()
-        scaleMatrix.setScale(ratioX, ratioY, middleX, middleY)
-
-        val canvas = Canvas(scaledBitmap!!)
-        canvas.matrix = scaleMatrix
-        canvas.drawBitmap(bmp, middleX - bmp.width / 2, middleY - bmp.height / 2, Paint(Paint.FILTER_BITMAP_FLAG))
-
-        val exif: ExifInterface
-        try {
-            exif = ExifInterface(filePath)
-            val orientation = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION, 0)
-            Log.d("EXIF", "Exif: $orientation")
-            val matrix = Matrix()
-            when (orientation) {
-                6 -> {
-                    matrix.postRotate(90f)
-                    Log.d("EXIF", "Exif: $orientation")
-                }
-                3 -> {
-                    matrix.postRotate(180f)
-                    Log.d("EXIF", "Exif: $orientation")
-                }
-                8 -> {
-                    matrix.postRotate(270f)
-                    Log.d("EXIF", "Exif: $orientation")
-                }
-            }
-            scaledBitmap = Bitmap.createBitmap(
-                scaledBitmap, 0, 0,
-                scaledBitmap.width, scaledBitmap.height, matrix,
-                true
-            )
-        } catch (e: IOException) {
-            e.printStackTrace()
-        }
-
-        val out: FileOutputStream?
-        val filename = getFilename()
-        try {
-            out = FileOutputStream(filename)
-            scaledBitmap!!.compress(Bitmap.CompressFormat.JPEG, 70, out)
-        } catch (e: FileNotFoundException) {
-            e.printStackTrace()
-        }
-        return filename
-
-    }
+//    fun compressImage(imageUri: String): String {
+//        val filePath = getRealPathFromURI(imageUri)
+//        var scaledBitmap: Bitmap? = null
+//
+//        val options = BitmapFactory.Options()
+//        options.inJustDecodeBounds = true
+//        var bmp = BitmapFactory.decodeFile(filePath, options)
+//
+//        var actualHeight = options.outHeight
+//        var actualWidth = options.outWidth
+//
+//        val maxHeight = 816.0f
+//        val maxWidth = 612.0f
+//        var imgRatio = (actualWidth / actualHeight).toFloat()
+//        val maxRatio = maxWidth / maxHeight
+//
+//        if (actualHeight > maxHeight || actualWidth > maxWidth) {
+//            when {
+//                imgRatio < maxRatio -> {
+//                    imgRatio = maxHeight / actualHeight
+//                    actualWidth = (imgRatio * actualWidth).toInt()
+//                    actualHeight = maxHeight.toInt()
+//                }
+//                imgRatio > maxRatio -> {
+//                    imgRatio = maxWidth / actualWidth
+//                    actualHeight = (imgRatio * actualHeight).toInt()
+//                    actualWidth = maxWidth.toInt()
+//                }
+//                else -> {
+//                    actualHeight = maxHeight.toInt()
+//                    actualWidth = maxWidth.toInt()
+//
+//                }
+//            }
+//        }
+//
+//        options.inSampleSize = calculateInSampleSize(options, actualWidth, actualHeight)
+//        options.inJustDecodeBounds = false
+//        options.inTempStorage = ByteArray(16 * 1024)
+//
+//        try {
+//            bmp = BitmapFactory.decodeFile(filePath, options)
+//        } catch (exception: OutOfMemoryError) {
+//            exception.printStackTrace()
+//
+//        }
+//
+//        try {
+//            scaledBitmap = Bitmap.createBitmap(actualWidth, actualHeight, Bitmap.Config.ARGB_8888)
+//        } catch (exception: OutOfMemoryError) {
+//            exception.printStackTrace()
+//        }
+//
+//        val ratioX = actualWidth / options.outWidth.toFloat()
+//        val ratioY = actualHeight / options.outHeight.toFloat()
+//        val middleX = actualWidth / 2.0f
+//        val middleY = actualHeight / 2.0f
+//
+//        val scaleMatrix = Matrix()
+//        scaleMatrix.setScale(ratioX, ratioY, middleX, middleY)
+//
+//        val canvas = Canvas(scaledBitmap!!)
+//        canvas.matrix = scaleMatrix
+//        canvas.drawBitmap(bmp, middleX - bmp.width / 2, middleY - bmp.height / 2, Paint(Paint.FILTER_BITMAP_FLAG))
+//
+//        val exif: ExifInterface
+//        try {
+//            exif = ExifInterface(filePath)
+//            val orientation = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION, 0)
+//            Log.d("EXIF", "Exif: $orientation")
+//            val matrix = Matrix()
+//            when (orientation) {
+//                6 -> {
+//                    matrix.postRotate(90f)
+//                    Log.d("EXIF", "Exif: $orientation")
+//                }
+//                3 -> {
+//                    matrix.postRotate(180f)
+//                    Log.d("EXIF", "Exif: $orientation")
+//                }
+//                8 -> {
+//                    matrix.postRotate(270f)
+//                    Log.d("EXIF", "Exif: $orientation")
+//                }
+//            }
+//            scaledBitmap = Bitmap.createBitmap(
+//                scaledBitmap, 0, 0,
+//                scaledBitmap.width, scaledBitmap.height, matrix,
+//                true
+//            )
+//        } catch (e: IOException) {
+//            e.printStackTrace()
+//        }
+//
+//        val out: FileOutputStream?
+//        val filename = getFilename()
+//        try {
+//            out = FileOutputStream(filename)
+//            scaledBitmap!!.compress(Bitmap.CompressFormat.JPEG, 70, out)
+//        } catch (e: FileNotFoundException) {
+//            e.printStackTrace()
+//        }
+//        return filename
+//
+//    }
 
     private fun getFilename(): String {
         val file = File(Environment.getExternalStorageDirectory().path, "MyFolder/Images")
